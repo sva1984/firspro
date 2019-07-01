1553a23979bc430bae825013132044b0a841ce87
sva1984 <rasev108@gmail.com>
2019-07-01 16:24:52 +0300

    v1

---
+++ form1.php
@@ -0,0 +1,6 @@
+
+<form action="index.php" method="post">
+ <p>Ваше имя: <input type="text" name="name" /></p>
+ <p>Ваш возраст: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
